d659c77539bdecafb44df38f4727a98c4ec43b36
NeethuSoman25 <neethusoman123@gmail.com>
2024-12-10 15:32:32 +0530

    new view

--- snackHouse/client/views.py
+++ snackHouse/client/views.py
@@ -8,7 +8,7 @@ from .models import Client,Snacks,Tables,Order,Payment,Contact,Otp
 import random
 from django.core.mail import send_mail
 from django.conf import settings
-# Create your views here.
+
 def index(request):
   return render(request,'nouser.html')
 
